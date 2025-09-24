8564e9d3ec3e73db436583bb057b29bf1a1f4ccd
Kamoliddin_Mirzoyev <k.mirzoyev@newuu.uz>
2025-09-24 05:15:18 +0000

    1st tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            margin: const EdgeInsets.all(20.0), // Added margin here
+            padding: const EdgeInsets.all(16.0),
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: const Icon(
+              Icons.home,
+              color: Colors.white,
+              size: 48,
+            ),
+          ),
         ),
       ),
     );
